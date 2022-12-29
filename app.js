52710bd3dd215c53d85b82dcb3b142f6fd3a0dd5
unknown <malismile786@gmail.com>
2022-12-29 19:05:21 +0500

    fix env

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-const PORT = process.env.PORT || 5000;
+const PORT = 8080 || process.env.PORT;
 
 app.listen(PORT, console.log(`Server running o  ${PORT}`));
 module.exports = app;
