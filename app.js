9bdbfa002c91e48fde982d60e3eb7da4318d00b5
unknown <malismile786@gmail.com>
2022-12-29 18:14:58 +0500

    add v3 fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 // DB Config
 const db = require('./config/keys').mongoURI;
+const PORT = require('./config/keys').PORT;
 
 app.use(express.json());
 app.use(cors());
@@ -25,20 +26,11 @@ app.use('/users', require('./routes/userRequest'));
 app.use('/admin', require('./routes/leavetype'));
 app.use('/admin', require('./routes/department'));
 
-const PORT = process.env.PORT || 5000;
+const PORT1 = process.env.PORT || PORT;
 
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-// Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-app.listen(PORT, console.log(`Server running o  ${PORT}`));
+app.listen(PORT1, console.log(`Server running o  ${PORT1}`));
 module.exports = app;
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:AjnxxN8dN1yi8w2g@crud-shard-00-00.zufth.mongodb.net:27017,crud-shard-00-01.zufth.mongodb.net:27017,crud-shard-00-02.zufth.mongodb.net:27017/PROJECT0?ssl=true&replicaSet=atlas-h0k5yk-shard-0&authSource=admin&retryWrites=true&w=majority',
-    jwtSecret: '123'
-}
+  mongoURI:
+    'mongodb://admin:AjnxxN8dN1yi8w2g@crud-shard-00-00.zufth.mongodb.net:27017,crud-shard-00-01.zufth.mongodb.net:27017,crud-shard-00-02.zufth.mongodb.net:27017/PROJECT0?ssl=true&replicaSet=atlas-h0k5yk-shard-0&authSource=admin&retryWrites=true&w=majority',
+  jwtSecret: '123',
+  PORT: 5000,
+};
