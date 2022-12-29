a7a9f461ed0f032ddca7989a5203bbdb4d7ad5cc
unknown <malismile786@gmail.com>
2022-12-29 18:59:23 +0500

    fix env

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 // DB Config
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys').mongoURI;
 
 app.use(express.json());
 app.use(cors());
 app.use(express.json({ type: 'application/*+json' }));
+const dotenv = require('dotenv');
+// config dot env file
+dotenv.config();
 
 // Connect to mongodb
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('connect to mongodb');
   })
