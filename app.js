735f513a71a62c7c60df36a2f74bb7a15a6d70c4
unknown <malismile786@gmail.com>
2022-12-29 18:42:01 +0500

    add v5 fix

--- app.js
+++ app.js
@@ -25,12 +25,11 @@ app.use('/users', require('./routes/userRequest'));
 app.use('/admin', require('./routes/leavetype'));
 app.use('/admin', require('./routes/department'));
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running o  ${PORT}`));
 module.exports = app;
